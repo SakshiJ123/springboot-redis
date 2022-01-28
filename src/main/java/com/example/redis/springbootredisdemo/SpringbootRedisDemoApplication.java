1a434992dc125bcac5d234b19892205b9f70e4ad
Sakshi Jogdand <Sakshi.Jogdand@ril.com>
2022-01-28 18:25:15 +0530

    Redis Integration in Spring Boot

--- src/main/java/com/example/redis/springbootredisdemo/SpringbootRedisDemoApplication.java
+++ src/main/java/com/example/redis/springbootredisdemo/SpringbootRedisDemoApplication.java
@@ -3,7 +3,6 @@ package com.example.redis.springbootredisdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
--- src/main/java/com/example/redis/springbootredisdemo/model/Employee.java
+++ src/main/java/com/example/redis/springbootredisdemo/model/Employee.java
@@ -1,15 +1,39 @@
 package com.example.redis.springbootredisdemo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class Employee implements Serializable {
-    private int id;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
     private String name;
+    
+	public Employee() {
+		super();
+	}
+	
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+    
 }
